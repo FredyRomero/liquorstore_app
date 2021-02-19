d0c9433b5360aa25220c357002fae0ee5920cf36
FredyRomero <fredysebastianromero@gmail.com>
2021-02-19 13:18:19 +0100

    eliminar codigo comentadoe innecesario

--- models/liquorstore_alcohol.py
+++ models/liquorstore_alcohol.py
@@ -8,13 +8,9 @@ class Alcohol(models.Model):
     name = fields.Char('Nombre', required=True)
     marca = fields.Char('Marca')
     descripcion = fields.Char('Descripcion')
-    image = fields.Binary('Image', help="../static/description/imgliquorstore.png")
+    image = fields.Image('Image')
     active = fields.Boolean('Active?', default=True)
     precio = fields.Integer('Precio',required=True)
-    # date_published = fields.Date()
-    # publisher_id = fields.Many2one('res.partner', string='Publisher')
-    # author_ids = fields.Many2many('res.partner', string='Authors')
-    # name = fields.Char(translate=True, required=True)
 
     # Hierarchy fields
     tipos_id = fields.Many2one(
--- models/liquorstore_alcohol_tipo.py
+++ models/liquorstore_alcohol_tipo.py
@@ -11,19 +11,3 @@ class AlcoholTipo(models.Model):
         'Parent Category',
         ondelete='restrict')
     parent_path = fields.Char(index=True)
-
-    # Optional but good to have:
-    parent_id = fields.Many2one(
-        'liquorstore.alcohol.tipo',
-        'Parent Category',
-        ondelete='restrict')
-
-    child_ids = fields.One2many(
-        'liquorstore.alcohol.tipo',
-        'parent_id',
-        'Subcategories')
-
-    highlighted_id = fields.Reference(
-        [('liquorstore.alcohol.tipo', 'Alcohol'), ('res.partner', 'Author')],
-        'Category Highlight'
-    )
